601df7f042f62f9820d531e07f5529a761dafc0c
Volya <woldemar1234512@gmail.com>
2020-09-17 22:21:16 +0300

    Нарисовал прямоугольные ячейки под числа массива

--- src/main/java/ru/startjava/sortbubbles/SortBubblesApp.java
+++ src/main/java/ru/startjava/sortbubbles/SortBubblesApp.java
@@ -16,7 +16,26 @@ public class SortBubblesApp extends GraphicsCore {
         graphics.drawString(String.valueOf(24), 45, 55);
 
         graphics.drawRect(80, 25, 50, 50);
-        graphics.drawString(String.valueOf(47),100, 55);
+        graphics.drawString(String.valueOf(47), 100, 55);
+
+        graphics.drawRect(135, 25, 50, 50);
+        graphics.drawString(String.valueOf(47), 155, 55);
+
+        graphics.drawRect(190, 25, 50, 50);
+        graphics.drawString(String.valueOf(47), 210, 55);
+
+        graphics.drawRect(245, 25, 50, 50);
+        graphics.drawString(String.valueOf(47), 265, 55);
+
+        graphics.drawRect(300, 25, 50, 50);
+        graphics.drawString(String.valueOf(47), 320, 55);
+
+        graphics.drawRect(355, 25, 50, 50);
+        graphics.drawString(String.valueOf(47), 375, 55);
+
+        graphics.drawRect(410, 25, 50, 50);
+        graphics.drawString(String.valueOf(47), 430, 55);
+
 
         // 2. Отобразить элементы массива в
         // прямоугольниках
@@ -25,18 +44,18 @@ public class SortBubblesApp extends GraphicsCore {
     }
 
     public static void main(String[] args) {
-        // 1. Вынести в функцию
+        // 4. Вынести в функцию
         boolean isSorted = false;
         int buf;
-        while(!isSorted) {
+        while (!isSorted) {
             isSorted = true;
-            for (int i = 0; i < mas.length-1; i++) {
-                if(mas[i] > mas[i+1]){
+            for (int i = 0; i < mas.length - 1; i++) {
+                if (mas[i] > mas[i + 1]) {
                     isSorted = false;
 
                     buf = mas[i];
-                    mas[i] = mas[i+1];
-                    mas[i+1] = buf;
+                    mas[i] = mas[i + 1];
+                    mas[i + 1] = buf;
                 }
             }
         }
