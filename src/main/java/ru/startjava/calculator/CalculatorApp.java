b116319096b9c6c59d05fe68d2f5dd14ff9a9dd4
Volya <woldemar1234512@gmail.com>
2020-10-03 20:58:10 +0300

    Реализовал определение позиции курсора на цифровой клавише

--- src/main/java/ru/startjava/calculator/CalculatorApp.java
+++ src/main/java/ru/startjava/calculator/CalculatorApp.java
@@ -5,14 +5,29 @@ import ru.startjava.core.GraphicsCore;
 import java.awt.*;
 
 public class CalculatorApp extends GraphicsCore {
+    private int x = 0;
+    private int y = 0;
+
+    private Point getCursorLocation() {
+        return MouseInfo.getPointerInfo()
+                .getLocation();
+    }
+
+    private int getCursorCoordinateX() {
+        return getCursorLocation().x;
+    }
+
+    private int getCursorCoordinateY() {
+        return getCursorLocation().y;
+    }
+
     @Override
     protected void showGraphics(Graphics graphics) {
         // Макет калькулятора
         graphics.setColor(Color.BLACK);
 
-        int num[] = new int[]{1,2,3,4,5,6,7,8,9,0};
-        char fun[] = new char[]{'+','-','*','/'};
-
+        int[] numbers = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+        char[] operations = new char[]{'+', '-', '*', '/'};
 
         int widthScreen = this.getWidth();
 
@@ -24,16 +39,16 @@ public class CalculatorApp extends GraphicsCore {
 
         int widthSquare = 50;
 
-         int posYY = 140;
+        int posYY = 140;
         int posYT = 175;
-        for (int j = 0; j < fun.length;j++){
-            graphics.drawRect(225,posYY,60,60);
-            graphics.drawString(String.valueOf(fun[j]),255,posYT);
+        for (int j = 0; j < operations.length; j++) {
+            graphics.drawRect(225, posYY, 60, 60);
+            graphics.drawString(String.valueOf(operations[j]), 255, posYT);
             posYY += 65;
             posYT += 65;
         }
 
-        for (int i = 0; i < num.length; i++) {
+        for (int i = 0; i < numbers.length; i++) {
             if (posX + widthSquare > widthScreen - 100) {
                 posX = 10;
                 posY += 65;
@@ -41,13 +56,19 @@ public class CalculatorApp extends GraphicsCore {
                 posXTeXt = 30;
                 posYText += 65;
             }
+            if ((getCursorCoordinateX() >= posX && getCursorCoordinateX() <= posX + 60) &&
+                    (getCursorCoordinateY() >= posY && getCursorCoordinateY() <= posY + 60)) {
+                System.out.println(" Мышь на клавише " + numbers[i]);
+            }
             graphics.drawRect(posX, posY, 60, 60);
-            graphics.drawString(String.valueOf(num[i]), posXTeXt, posYText);
+            graphics.drawString(String.valueOf(numbers[i]), posXTeXt, posYText);
             posX += 65;
-            posXTeXt +=65;
+            posXTeXt += 65;
         }
-        graphics.drawRect(75,335,125,60);
-        graphics.drawString("=",135,370);
+        graphics.drawRect(75, 335, 125, 60);
+        graphics.drawString("=", 135, 370);
+
+        graphics.drawRect(10, 50, widthScreen - 25, 50);
     }
 
     public static void main(String[] args) {
--- src/main/java/ru/startjava/core/GraphicsCore.java
+++ src/main/java/ru/startjava/core/GraphicsCore.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public abstract class GraphicsCore extends JPanel implements ActionListener {
-    private final int delay = 1500;
+    private final int delay = 100;
     private final int width = 300;
     private final int height = 400;
     private Timer animationTimer;
